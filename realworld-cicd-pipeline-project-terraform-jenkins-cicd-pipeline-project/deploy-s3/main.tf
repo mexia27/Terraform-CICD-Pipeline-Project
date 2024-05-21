1434a448fb4775af5b48470e06f844ff1a0cd3d7
mexia27 <59975829+mexia27@users.noreply.github.com>
2024-05-20 23:39:01 -0400

    Delete realworld-cicd-pipeline-project-terraform-jenkins-cicd-pipeline-project directory

--- realworld-cicd-pipeline-project-terraform-jenkins-cicd-pipeline-project/deploy-s3/main.tf
+++
@@ -1,9 +0,0 @@
-# Resources = Bucket resource
-resource "aws_s3_bucket" "mbandi_dev_bucket" {
-  bucket = "mbandi-dev-bucket-126543279054"
-  tags = {
-    Environment = "dev"
-    CostCenter  = "cc590"
-  }
-}
-
